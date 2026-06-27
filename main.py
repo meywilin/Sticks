efe77c86897fbc14ec28b0335c3d1e8e17e4c0fc
Druzhinina Mariia <madruzhinina2011@mail.ru>
2026-06-27 09:37:29 +0300

    function play_game()

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
+from utils import check_winner
+from app_logic import human_turn, computer_turn
+
 def start_game():
-    stickes=21
+    sticks=21
     turn=input("Хотите ходить первым? (да/нет)").strip().lower()
     if turn == "да":
         human_first = True
     else:
         human_first = False
     play_game(human_first, sticks)
+
+def play_game(human_first, sticks):
+    current_player=human_first
+    while sticks>0:
+        if current_player:
+            taken = human_turn(sticks)
+        else:
+            taken = computer_turn(sticks)
+        sticks-=taken
+        current_player = not current_player
+        if check_winner(sticks):
+            winner = "Вы выиграли!" if current_player else "Победил компьютер!"
+            print(winner)
+            break
+if __name__=="__main__":
+    import random
+    start_game()
