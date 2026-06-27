62b148795224b14b5b9e7c0e3f704ed7a52581b7
Druzhinina Mariia <madruzhinina2011@mail.ru>
2026-06-27 09:29:53 +0300

    function computer_turn

--- app_logic.py
+++ app_logic.py
@@ -1,4 +1,5 @@
-def human_turn(srucks_left):
+import random
+def human_turn(sticks_left):
     while True:
         try:
             take=int(input(f"Ваш ход. Осталось {sticks_left}. Сколько возьмете? (1-3)"))
@@ -8,3 +9,11 @@ def human_turn(srucks_left):
                 pritn("Ошибка можно взять от 1 до 3 спичек")
         except ValueError:
             print("Ошибка, введите целое число")
+
+def computer_turn(sticks_left):
+        if sticks_left >= 4 and sticks_left%4 != 0:
+            computer_take = sticks_left%4
+        else:
+            computer_take = random.randint(1, min(3, sticks_left))
+        print(f"Компьютер берет {computer_take} спичек")
+        return computer_take
